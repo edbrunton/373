c1465d2dbd82c0cad7cd48d70671e3f7f4ab211d
edbrunton <edbrunton@email.arizona.edu>
2017-11-07 16:27:24 -0700

    Error handling

--- src/gui/NewAccountNewCustomerGUI.java
+++ src/gui/NewAccountNewCustomerGUI.java
@@ -1,16 +1,22 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -46,22 +52,129 @@ public class NewAccountNewCustomerGUI extends JFrame   {
 		public void actionPerformed(ActionEvent e)
 		{
 			String firstName= c1.getText();
+			try {	
+				if (firstName.length() == 0)
+				{
+					throw new Exception("error");	
+				}
+			}
+			catch(Exception p)
+			{
+				String title = "Error First Name Not Valid";
+				String message ="Enter a valid first name, scum";
+				textParseError(title, message);
+			}
 			String lastName = c2.getText();
+			try {			
+				if (lastName.length() == 0)
+				{
+					throw new Exception("error");	
+				}
+			}
+			catch(Exception p)
+			{
+				String title = "Error Last Name Not Valid";
+				String message ="Enter a valid last name, scum";
+				textParseError(title, message);
+			}
 			String address = c3.getText();
-			String zipode = c4.getText();
-			String phoneNum = c5.getText();
+			try {
+				
+				if (address.length() == 0)
+				{
+					throw new Exception("error");	
+				}
+			}
+			catch(Exception p)
+			{
+				String title = "Error Address Not Valid";
+				String message ="Enter a valid address, scum";
+				textParseError(title, message);
+			}
+			int zipcode;
+			try {
+				zipcode = Integer.parseInt(c4.getText());
+				if(c4.getText().length() != 5)
+				{
+					throw new Exception("error");
+				}
+			}
+			catch(Exception p)
+			{
+				String title = "Error Zipcode Not Valid";
+				String message ="Enter a valid zipcode, scum";
+				textParseError(title, message);
+			}
+			int phoneNum;
+			try{
+				phoneNum = Integer.parseInt(c5.getText());
+				if(c5.getText().length() > 12 || c5.getText().length() < 7)
+				{
+					throw new Exception("error");
+				}
+			}
+			catch(Exception p)
+			{
+				String title = "Error Phone Number Not Valid";
+				String message ="Enter a valid phone number, scum";
+				textParseError(title, message);
+			}
 			String accountType = (String)f1.getSelectedItem();
 			String loanAmount = f2.getText();
 		//	String SSPreCheck = f3.getPassword().toString();
 			String email = f4.getText();
+			try {
+				if(email.contains("@") == false)
+				{
+					throw new Exception("error");
+				}
+			}
+			catch(Exception p){
+				String title = "Error Email Address Not Valid";
+				String message ="Enter a valid Email Address number, scum";
+				textParseError(title, message);
+			}
 			String password = f5.getPassword().toString();
+			try {
+				if(password.length() < 8)
+				{
+					throw new Exception("error");	
+				}
+			}
+			catch(Exception p){
+				String title = "Error Password Not Valid";
+				String message ="Enter a password of at least eight characters moron";
+				textParseError(title, message);
+			}
 			int socialSecurity;
 			try {
 				socialSecurity = Integer.parseInt(f3.getPassword().toString());
+				if(f3.getPassword().toString().length() != 9)
+				{
+					throw new Exception("error");
+				}
 			}
 			catch(Exception p){
-				//need to throw error
+				String title = "Error Social Security Number Not Valid";
+				String message ="Enter a valid Social Security number, scum";
+				textParseError(title, message);
 			}
+		}
+
+		private void textParseError(String title, String message) {
+			JDialog frameTemp = new JDialog (new JFrame(), title);//inspired by https://stackoverflow.com/questions/2665355/how-do-i-remove-the-maximize-and-minimize-buttons-from-a-jframe  
+			        Container contentPane = frameTemp.getContentPane ();
+			        contentPane.setLayout (new BorderLayout());
+			        JButton okay = new JButton("OK");
+			        okay.addActionListener(e2 -> frameTemp.dispose());
+			        okay.setSize(30, 10);
+			        JPanel p1 = new JPanel();
+			        p1.add(okay);
+			        contentPane.add(p1, BorderLayout.SOUTH);			    	        
+			        JLabel outArea = new JLabel(message);
+			        contentPane.add(outArea, BorderLayout.CENTER);
+			        frameTemp.pack();
+			        frameTemp.setVisible (true);		//need to throw error
 		}
 	}
 	private void buildGUI() {
